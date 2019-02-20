2d30a3504b881d612b0647225baa6681edb64a4e
ct <ctaylr13@gmail.com>
2019-02-20 16:04:06 -0500

    Update index.js

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    let max = 0;
+    let maxChar = '';
 
+    for (let x of str) {
+        if(chars[x]) {
+            chars[x]++;
+        } else {
+            chars[x] = 1;
+        }
+    }
+    // console.log("CHARS Object", chars);
+
+    const entries = Object.entries(chars);
+    // console.log("entries", entries);
+
+    for (let x in chars) {
+        if (chars[x] > max) {
+            maxChar = x;
+            max = chars[x]; 
+        }
+    }
+
+    return maxChar;
+
+    // return str;
+}
+
+console.log(maxChar("test"));
 module.exports = maxChar;
