86a567e02dc48a73102276c82dd95734829c8558
ct <ctaylr13@gmail.com>
2019-02-20 20:05:32 -0500

    chunk solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // Create empty array to hold chuncks called 'total'
+    const total = []; 
+    // for each element in Array
+    for (let x of array) {
+        // retrieve the last element in total
+        const last = total[total.length - 1];
+        // if last element does not exist or if its length is equal to chunck size 
+        if (!last || last.length === size) {
+            // push a new chunk into total with the current element 
+            total.push([x]);         
+        // add the current element to the array 
+        } else {
+            last.push(x);
+        }
+    }
+    return total;
+}
+
+
 
 module.exports = chunk;
